25d48a233bc34ca745835d71903b0d40a37f8d05
かせいさん <kasei.san@gmail.com>
2013-03-22 14:26:59 +0900

    デモを追加

--- presentation.js
+++ presentation.js
@@ -13,5 +13,33 @@ io.sockets.on('connection', function(socket){
   });
 });
 
-
-
+var Field = function(){
+  this.X_MAX=640;
+  this.Y_MAX=480;
+  this.BALL_SIZE=20;
+  this.position = {
+    x : this.X_MAX/2 - this.BALL_SIZE/2,
+    y : this.Y_MAX/2 - this.BALL_SIZE/2
+  }
+  this.vector = {x : 5, y : 5}
+}
+Field.prototype = {
+  next : function(){
+    this.position.x += this.vector.x;
+    this.position.y += this.vector.y;
+    if(this.position.x >= (this.X_MAX-(this.BALL_SIZE*2))){ this.position.x = this.X_MAX-(this.BALL_SIZE*2); this.vector.x = this.vector.x * -1; }
+    if(this.position.y >= (this.Y_MAX-(this.BALL_SIZE*2))){ this.position.y = this.Y_MAX-(this.BALL_SIZE*2); this.vector.y = this.vector.y * -1; }
+    if(this.position.x <= 0){ this.position.x = 0; this.vector.x = this.vector.x * -1; }
+    if(this.position.y <= 0){ this.position.y = 0; this.vector.y = this.vector.y * -1; }
+    return {
+      ball : this.position
+    };
+  }
+}
+var field = new Field
+var pong_io = require('socket.io').listen(3001);
+pong_io.sockets.on('connection', function (socket) {
+  setInterval(function(){
+    socket.broadcast.emit("data", field.next());
+  }, 100);
+});
